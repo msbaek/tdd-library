2a3f8b67d387b6ba0c013cdec4c1ba3682ddf4e4
Myeongseok Baek <codetemplate@hanmail.net>
2024-03-08 16:57:30 +0900

    add WebMvcTest

---
+++ src/test/java/pe/msbaek/tddcases/bookloan/loan/RegisterBookWebMvcTest.java
@@ -0,0 +1,39 @@
+package pe.msbaek.tddcases.bookloan.loan;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BookController.class)
+public class RegisterBookWebMvcTest {
+    @Autowired private MockMvc mockMvc;
+    @MockBean private BookRepository bookRepository;
+    @Autowired private ObjectMapper objectMapper;
+
+    @Test
+    void register_book_successfully() throws Exception {
+        String publishedDateString = "1925-04-10";
+        BookController.Request request = new BookController.Request("The Great Gatsby", "F. Scott Fitzgerald", publishedDateString);
+        Book book = new Book("The Great Gatsby", "F. Scott Fitzgerald", LocalDate.parse(publishedDateString));
+        given(bookRepository.save(Mockito.<Book>any())).willReturn(book);
+
+        mockMvc.perform(post("/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("The Great Gatsby")));
+    }
+}
