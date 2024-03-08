362c08fd061ee990c9de992740464594993c4d80
Myeongseok Baek <codetemplate@hanmail.net>
2024-03-08 16:01:49 +0900

    project created

---
+++ src/main/java/pe/msbaek/tddcases/bookloan/BookLoanApplication.java
@@ -0,0 +1,13 @@
+package pe.msbaek.tddcases.bookloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookLoanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/msbaek/tddcases/bookloan/BookLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.msbaek.tddcases.bookloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
