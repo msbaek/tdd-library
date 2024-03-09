51872867797a6b3d916de4764bf08234ac82e7f1
Myeongseok Baek <codetemplate@hanmail.net>
2024-03-09 13:48:06 +0900

    use jakarta valiadtor

--- src/main/java/pe/msbaek/tddcases/bookloan/loan/BookController.java
+++ src/main/java/pe/msbaek/tddcases/bookloan/loan/BookController.java
@@ -1,5 +1,8 @@
 package pe.msbaek.tddcases.bookloan.loan;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,17 +18,14 @@ public class BookController {
     private final BookRepository bookRepository;
 
     @PostMapping("/books")
-    public ResponseEntity<Response> addBook(@RequestBody Request request) {
+    public ResponseEntity<Response> addBook(@RequestBody @Valid Request request) {
         // validate
-        if (request.title() == null || request.title().isBlank()) {
-            throw new IllegalArgumentException("title is required");
-        }
-        // save
         bookRepository.findByTitle(request.title())
                 .ifPresent(book -> {
                     throw new IllegalArgumentException("title is already exists");
                 });
 
+        // save
         LocalDate publishedDate = LocalDate.parse(request.publishedDate());
         bookRepository.save(new Book(request.title(), request.author(), publishedDate));
         Response response = new Response(request.title(), request.author(), request.publishedDate());
@@ -34,6 +34,13 @@ public class BookController {
                 HttpStatus.OK);
     }
 
-    record Request(String title, String author, String publishedDate) {}
+    record Request(
+            @NotBlank(message = "title is required")
+            String title,
+            @NotBlank(message = "author is required")
+            String author,
+            @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "publishedDate should have yyyy-MM-dd format")
+            String publishedDate
+    ) {}
     record Response(String title, String author, String publishedDate) {}
 }
--- src/main/java/pe/msbaek/tddcases/bookloan/loan/ExceptionAdvice.java
+++ src/main/java/pe/msbaek/tddcases/bookloan/loan/ExceptionAdvice.java
@@ -3,6 +3,7 @@ package pe.msbaek.tddcases.bookloan.loan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,8 +11,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionAdvice {
     @ExceptionHandler(IllegalArgumentException.class)
     protected ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
-        ErrorResponse errorResponse = ErrorResponse.builder(e, HttpStatus.BAD_REQUEST, e.getMessage())
-                .build();
+        return getErrorResponseEntity(ErrorResponse.builder(e, HttpStatus.BAD_REQUEST, e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return getErrorResponseEntity(ErrorResponse.builder(e, HttpStatus.BAD_REQUEST, e.getMessage()));
+    }
+
+    private ResponseEntity<String> getErrorResponseEntity(ErrorResponse.Builder e) {
+        ErrorResponse errorResponse = e.build();
         return new ResponseEntity<>(errorResponse.toString(), HttpStatus.BAD_REQUEST);
     }
 }
